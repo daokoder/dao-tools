c771b22c3a57eeb811d2f29709d90f1a298ec17c
Limin Fu <daokoder@gmail.com>
2012-06-21 06:23:30 +0000

    Add minor changes.

--- clangdao/clangdao.cpp
+++ clangdao/clangdao.cpp
@@ -243,8 +243,6 @@ string cdao_make_dao_template_type_name( const string & name0, const map<string,
 				string quote = is_invalid_dao_type_name( part ) ? "'" : "";
 				it = subs.find( part );
 				if( it != subs.end() ) part = it->second;
-				if( part.find( "std::" ) == 0 ) part.replace( 0, 5, "_std::" );
-				if( part.find( "io::" ) == 0 ) part.replace( 0, 4, "_io::" );
 				if( type_for_quoting.find( part ) != type_for_quoting.end() ) quote = "'";
 				if( type_for_quoting.size() == 0 ) quote = "";
 				result += quote + part + quote;
@@ -260,8 +258,6 @@ string cdao_make_dao_template_type_name( const string & name0, const map<string,
 	string quote = is_invalid_dao_type_name( part ) ? "'" : "";
 	it = subs.find( part );
 	if( it != subs.end() ) part = it->second;
-	if( part.find( "std::" ) == 0 ) part.replace( 0, 5, "_std::" );
-	if( part.find( "io::" ) == 0 ) part.replace( 0, 4, "_io::" );
 	if( type_for_quoting.find( part ) != type_for_quoting.end() ) quote = "'";
 	return result + quote + part + quote;
 }
@@ -278,8 +274,6 @@ string cdao_make_dao_template_type_name( const string & name0 )
 				string quote = is_invalid_dao_type_name( part ) ? "'" : "";
 				it = type_substitutions.find( part );
 				if( it != type_substitutions.end() ) part = it->second;
-				if( part.find( "std::" ) == 0 ) part.replace( 0, 5, "_std::" );
-				if( part.find( "io::" ) == 0 ) part.replace( 0, 4, "_io::" );
 				if( type_for_quoting.find( part ) != type_for_quoting.end() ) quote = "'";
 				if( istemplate == 0 ) quote = "";
 				result += quote + part + quote;
@@ -295,8 +289,6 @@ string cdao_make_dao_template_type_name( const string & name0 )
 	string quote = is_invalid_dao_type_name( part ) ? "'" : "";
 	it = type_substitutions.find( part );
 	if( it != type_substitutions.end() ) part = it->second;
-	if( part.find( "std::" ) == 0 ) part.replace( 0, 5, "_std::" );
-	if( part.find( "io::" ) == 0 ) part.replace( 0, 4, "_io::" );
 	if( type_for_quoting.find( part ) != type_for_quoting.end() ) quote = "'";
 	if( istemplate == 0 ) quote = "";
 	return result + quote + part + quote;
